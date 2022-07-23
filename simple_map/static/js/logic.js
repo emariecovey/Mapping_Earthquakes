7f9613fcbb8d6165267d93c630810abca4390534
Emarie Swenson <emarieswenson@Emaries-MacBook-Air.local>
2022-07-22 22:12:01 -0600

    adding code to make markers and circles on map

--- simple_map/static/js/logic.js
+++ simple_map/static/js/logic.js
@@ -12,3 +12,23 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Marker for SLC just for fun
+let marker2 = L.marker([40.7608, -111.8910]).addTo(map);
+
+// Add circle to map for LA, CA (same as marker but with radius)
+// radius is measured in meters (so 100 is 100 meter radius)
+let marker = L.circle([34.0522, -118.2437], {
+    radius:300,
+    color: 'yellow',
+    
+}).addTo(map);
+
+// Add circle to NYC. Filled with light yellow, black around radius, large. good with dark-v10 style
+let marker3 = L.circleMarker([40.7128, -74.0060], {
+    radius:300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
+
