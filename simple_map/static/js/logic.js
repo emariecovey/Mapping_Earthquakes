14a59abc2c2c3985985ec6ac0228da4cfdcd26b1
Emarie Swenson <emarieswenson@Emaries-MacBook-Air.local>
2022-07-23 13:10:53 -0600

    adding multiple points on a map with circle markers proportional to pop size. Also, popups.

--- simple_map/static/js/logic.js
+++ simple_map/static/js/logic.js
@@ -3,8 +3,8 @@ console.log("something is happening");
 //create map object with center of US at center and zoom level (4 on a scale from 0-18)
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//add tile layer for map (background of map)
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//add tile layer for map (background of map) (dark-v10 is dark, regular is streets-v11)
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -14,21 +14,20 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 streets.addTo(map);
 
 // Marker for SLC just for fun
-let marker2 = L.marker([40.7608, -111.8910]).addTo(map);
+//let marker2 = L.marker([40.7608, -111.8910]).addTo(map);
+  
+//get cities data
+let cityData = cities;
 
-// Add circle to map for LA, CA (same as marker but with radius)
-// radius is measured in meters (so 100 is 100 meter radius)
-let marker = L.circle([34.0522, -118.2437], {
-    radius:300,
-    color: 'yellow',
+cityData.forEach(city => {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        lineweight: 4
+    })
+    //the hr inserts a horizontal row. The .toLocalString formats population to have commas every 3 digits
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
     
-}).addTo(map);
-
-// Add circle to NYC. Filled with light yellow, black around radius, large. good with dark-v10 style
-let marker3 = L.circleMarker([40.7128, -74.0060], {
-    radius:300,
-    color: "black",
-    fillColor: '#ffffa1'
-}).addTo(map);
-
-
